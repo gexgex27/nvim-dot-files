e2c7e722db6d66b3d1883cc1c76eb80c5fdea7ff
explo <alxsky@yandex.ru>
2023-04-22 16:22:53 +0700

    Изменил поведение автодополнения

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,27 +1,23 @@
 local status_bl, bufferline = pcall(require, "bufferline")
 if not status_bl then
-  return
+	return
 end
 
 bufferline.setup({
-  options = {
---    numbers = "none",
-    separator_style = "padded_slant",
---    show_tab_indicators = true,
---    show_buffer_close_icons = false,
---    show_close_icon = false,
-    offsets = {
-      {
-        filetype = "NvimTree",
-        text = "File Explorer",
-        text_align = "left",
-        separator = true,
-      },
-    },
-  },
-  highlights = {
-    buffer_selected = {
-      italic = false,
-    },
-  },
+	options = {
+		separator_style = "slant",
+		offsets = {
+			{
+				filetype = "NvimTree",
+				text = "File Explorer",
+				text_align = "left",
+				separator = true,
+			},
+		},
+	},
+	highlights = {
+		buffer_selected = {
+			italic = false,
+		},
+	},
 })
--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,75 +1,91 @@
 local status_cmp, cmp = pcall(require, "cmp")
 if not status_cmp then
-  print("Cmp не подключен!")
-  return
+	print("Cmp не подключен!")
+	return
 end
 
 local status_luasnip, luasnip = pcall(require, "luasnip")
 if not status_luasnip then
-  print("Luasnip не подключен!")
-  return
+	print("Luasnip не подключен!")
+	return
 end
 
 local status_lspkind, lspkind = pcall(require, "lspkind")
 if not status_lspkind then
-  print("Lspkind не подключен!")
-  return
+	print("Lspkind не подключен!")
+	return
 end
 
 require("luasnip/loaders/from_vscode").lazy_load()
 
 cmp.setup({
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
-    end,
-  },
-  mapping = cmp.mapping.preset.insert({
-    ["<C-k>"] = cmp.mapping.select_prev_item(),
-    ["<C-j>"] = cmp.mapping.select_next_item(),
-    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-    ["<C-f>"] = cmp.mapping.scroll_docs(4),
-    ["<C-Space>"] = cmp.mapping.complete(),
-    ["<C-e>"] = cmp.mapping.abort(),
-    ["<CR>"] = cmp.mapping.confirm({ select = false }),
-    ["<Tab>"] = cmp.mapping(function(fallback)
-      local entry = cmp.get_selected_entry()
-      if cmp.visible() then
-        cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
-      elseif not entry then
-        fallback()
-      else
-        cmp.complete()
-      end
-    end, {'i', 's'}),
-    ['<S-Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
-      else
-        fallback()
-      end
-    end, {'i', 's'}),
-  }),
-  sources = cmp.config.sources({
-    { name = "nvim_lsp" },
-    { name = "luasnip" },
-    { name = "buffer" },
-    { name = "path" },
-    { name = "treesitter" },
-  }),
-  formatting = {
-    format = lspkind.cmp_format({
-      mode = "symbol_text",
-      menu = ({
-        buffer = "[Buffer]",
-        nvim_lsp = "[LSP]",
-        luasnip = "[LuaSnip]",
-        nvim_lua = "[Lua]",
-        latex_symbols = "[Latex]",
-        treesitter = "[Treesitter]"
-      }),
-      maxwidth = 30,
-      ellipsis_char = "...",
-    }),
-  },
+	snippet = {
+		expand = function(args)
+			luasnip.lsp_expand(args.body)
+		end,
+	},
+	mapping = cmp.mapping.preset.insert({
+		["<Up>"] = cmp.mapping(function()
+			if cmp.visible() then
+				cmp.abort()
+				vim.api.nvim_command("normal k")
+			else
+				vim.api.nvim_command("normal k")
+			end
+		end, { "i", "s" }),
+		["<Down>"] = cmp.mapping(function()
+			if cmp.visible() then
+				cmp.abort()
+				vim.api.nvim_command("normal j")
+			else
+				vim.api.nvim_command("normal j")
+			end
+		end, { "i", "s" }),
+		["<C-k>"] = cmp.mapping.select_prev_item(),
+		["<C-j>"] = cmp.mapping.select_next_item(),
+		["<C-b>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<C-Space>"] = cmp.mapping.complete(),
+		["<C-e>"] = cmp.mapping.abort(),
+		["<CR>"] = cmp.mapping.confirm({ select = false }),
+		["<Tab>"] = cmp.mapping(function(fallback)
+			local entry = cmp.get_selected_entry()
+			if cmp.visible() then
+				cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+			elseif not entry then
+				fallback()
+			else
+				cmp.complete()
+			end
+		end, { "i", "s" }),
+		["<S-Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+	}),
+	sources = cmp.config.sources({
+		{ name = "nvim_lsp" },
+		{ name = "luasnip" },
+		{ name = "buffer" },
+		{ name = "path" },
+		{ name = "treesitter" },
+	}),
+	formatting = {
+		format = lspkind.cmp_format({
+			mode = "symbol_text",
+			menu = {
+				buffer = "[Buffer]",
+				nvim_lsp = "[LSP]",
+				luasnip = "[LuaSnip]",
+				nvim_lua = "[Lua]",
+				latex_symbols = "[Latex]",
+				treesitter = "[Treesitter]",
+			},
+			maxwidth = 30,
+			ellipsis_char = "...",
+		}),
+	},
 })
